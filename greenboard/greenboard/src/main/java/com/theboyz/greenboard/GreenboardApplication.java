8508b9268904fd405c56b63a9996f3ebf383676d
Vyacheslav Kim <kimslava@unist.ac.kr>
2023-07-30 20:55:21 +0600

    Maven project is added

---
+++ greenboard/greenboard/src/main/java/com/theboyz/greenboard/GreenboardApplication.java
@@ -0,0 +1,13 @@
+package com.theboyz.greenboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenboardApplication.class, args);
+	}
+
+}
---
+++ greenboard/greenboard/src/test/java/com/theboyz/greenboard/GreenboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theboyz.greenboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
